b369468b1150e346f593add621b652cf3be35f8e
programminghouse588 <thehouseofprogramming588@gmail.com>
2024-06-01 15:54:00 +0600

    Created initial server

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rbwl5qc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a new MongoClient
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Techno Zen server side is running on...");
+});
+
+app.listen(port, () => {
+  console.log(`Techno Zen running on http://localhost:${5000}`);
+});
