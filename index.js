c89fd31c71f0263eb486116205456035f534ff44
programminghouse588 <thehouseofprogramming588@gmail.com>
2024-06-09 03:39:58 +0600

    delete operation

--- index.js
+++ index.js
@@ -438,6 +438,14 @@ async function run() {
       }
     });
 
+    // delete my added food
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await ProductCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
